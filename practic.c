f73b2a5fde7cbdf8d39653f3803609b2d2af02d4
Don Pullen <donpullen@Dons-MacBook-Pro.local>
2020-07-22 19:59:49 -0400

    adding to numbers before branch

--- practic.c
+++ practic.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main (void)
 {
-  printf("hello world"\n);
+  char *name = malloc(20);
+
+  if (name == NULL)
+  {
+    printf("you have an error allocating memory\n");
+    return 1;
+  }
+  char dob[20];
+  char number[20];
+
+  printf("Enter in your name:\n");
+  fgets(name, 20, stdin);
+
+  printf("Enter in your dob:\n");
+  scanf("%s",dob);
+
+  printf("Enter in your number");
+  scanf("%s", number);
+
+  printf("%s - %s - %s \n",name,dob,number);
+
+  free(name);
 }
