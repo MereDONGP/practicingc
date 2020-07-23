0f2e154789b6e159401fb536f06cd408ad8d4121
Don Pullen <donpullen@Dons-MacBook-Pro.local>
2020-07-22 20:58:36 -0400

    created allocations

--- practic.c
+++ practic.c
@@ -11,19 +11,32 @@ int main (void)
     printf("you have an error allocating memory\n");
     return 1;
   }
-  char dob[20];
-  char number[20];
+  char *dob = malloc(20);
+
+  if (dob == NULL)
+  {
+    printf("something went wrong");
+    return 1;
+  }
+
+  char *number = malloc(20);
+  if (number == NULL)
+  {
+    return 1;
+  }
 
   printf("Enter in your name:\n");
   fgets(name, 20, stdin);
 
   printf("Enter in your dob:\n");
-  scanf("%s",dob);
+  fgets(dob, 20, stdin);
 
-  printf("Enter in your number");
-  scanf("%s", number);
+  printf("Enter in your number:\n");
+  fgets(number, 20, stdin);
 
-  printf("%s - %s - %s \n",name,dob,number);
+  printf("Name: %s \n Dob: %s \n Number %s \n",name,dob,number);
 
   free(name);
+  free(dob);
+  free(number);
 }
