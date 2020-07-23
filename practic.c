e519c564352a61aa1566cdcd90fd1be301da21ef
Don Pullen <donpullen@Dons-MacBook-Pro.local>
2020-07-23 19:46:22 -0400

    Did it with a structure

--- practic.c
+++ practic.c
@@ -15,14 +15,26 @@ int main (void)
 {
   people person[2];
 
+  char user[50];
+  printf("Enter in a name that you would like to search for\n");
+  fgets(user,50,stdin);
+
   person[0].name = "Don Pullen";
   person[0].number = "201 926 0483";
   person[0].DOB = "12/31/1996";
 
-  person[1].name = "Charlie Something";
-  person[1].number = "201-926-0483";
+  person[1].name = "Charlie";
+  person[1].number = "210-314-5424";
   person[1].DOB = "01/31/2001";
 
-  printf("%s\n",person[0].name);
-  printf("%s\n",person[1].name);
+  for (int i = 0; i < 2; i++)
+  {
+    if (strcmp(person[i].name, strtok(user,"\n")) == 0)
+    {
+      printf("%s\n%s\n",person[i].number, person[i].DOB);
+      return 0;
+    }
+
+  }
+    printf("nothing\n");
 }
